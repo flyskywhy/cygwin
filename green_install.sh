8b7a2e1d6a74ce5a2b454be23ea17ffedd685586
Li Zheng <flyskywhy@gmail.com>
2013-05-20 10:49:18 +0800

    green_install.sh: OK now

--- green_install.sh
+++ green_install.sh
@@ -1,2 +1,15 @@
-find /bin -name "*.exe" | xargs file | grep 012 | sed "s/:.*//g" | xargs attrib +s
-find /usr -name "*.exe" | xargs file | grep 012 | sed "s/:.*//g" | xargs attrib +s
+#! /bin/sh
+#
+# Li Zheng <flyskywhy@gmail.com>
+# 2013.5.20
+
+set_attrib()
+{
+    all=`find $1 -name "*.exe" | xargs file | grep 012 | sed "s/:.*//g"`
+    for i in $all; do
+        attrib +s $i
+    done
+}
+
+set_attrib ./bin
+set_attrib ./usr
