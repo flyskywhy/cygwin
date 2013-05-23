bf20f15f8ec715a50d212c159c2918536c5956c9
Li Zheng <flyskywhy@gmail.com>
2013-05-23 14:14:24 +0800

    green_install.sh: Add .a and .la

--- green_install.sh
+++ green_install.sh
@@ -3,7 +3,7 @@
 # Li Zheng <flyskywhy@gmail.com>
 # 2013.5.20
 
-set_attrib()
+set_attrib_on_exe()
 {
     all=`find $1 -name "*.exe" | xargs file | grep 012 | sed "s/:.*//g"`
     for i in $all; do
@@ -11,5 +11,24 @@ set_attrib()
     done
 }
 
-set_attrib ./bin
-set_attrib ./usr
+set_attrib_on_a()
+{
+    all=`find $1 -name "*.a" | xargs file | grep 012 | sed "s/:.*//g"`
+    for i in $all; do
+        attrib +s $i
+    done
+}
+
+set_attrib_on_la()
+{
+    all=`find $1 -name "*.la" | xargs file | grep 012 | sed "s/:.*//g"`
+    for i in $all; do
+        attrib +s $i
+    done
+}
+
+set_attrib_on_exe ./bin
+set_attrib_on_exe ./usr
+set_attrib_on_a ./usr
+set_attrib_on_a ./lib
+set_attrib_on_la ./lib
